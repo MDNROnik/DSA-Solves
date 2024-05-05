04d6bad29bdc302633950f500191fc6f1e62ace0
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-05-05 08:55:08 +0600

    Time: 9 ms (11.59%) | Memory: 13.2 MB (37.09%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        map<int,int>mp;
+        int n=nums1.size();
+        for(int i=0;i<n;i++){
+            mp[ nums1[i] ] ++ ;
+        }
+        n=nums2.size();
+        for(int i=0;i<n;i++){
+            if( mp[ nums2[i] ] ){
+                ans.push_back( nums2[i] );
+                mp[ nums2[i] ]--; 
+            }
+        }
+        return ans;
+    }
+};
