018a8c7efaab897b40bf781241cb11474e441752
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-05-05 08:59:59 +0600

    Time: 0 ms (100.00%) | Memory: 7.4 MB (77.51%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a=0,l=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='A'){
+                a++;l=0;
+            }
+            else if(s[i]=='L'){
+                l++;
+                if(l==3){return 0;}
+            }
+            else {l=0;}
+            if(a>=2 ){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
