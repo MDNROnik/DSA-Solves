2274072bf6144f646355700a4b082a8968ccdc0a
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-05-17 11:07:50 +0600

    Time: 131 ms (50.15%) | Memory: 73.7 MB (8.85%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head1 =  list1;
+        ListNode* head2 =  list2;
+        ListNode* head = new ListNode(-1);
+        ListNode* h=head;
+        while( head1 != NULL && head2 != NULL ){
+            if(head1->val==head2->val){
+                head->next=head1; head=head->next; head1=head1->next;
+                head->next=head2; head=head->next; head2=head2->next;
+            }
+            else if(head1->val < head2->val){
+                head->next=head1; head=head->next; head1=head1->next;
+            }
+            else{
+                head->next=head2; head=head->next; head2=head2->next;
+            }
+        }
+
+        while( head1 != NULL  ){
+            head->next=head1; head=head->next; head1=head1->next;
+        }
+
+        while( head2 != NULL  ){
+            head->next=head2; head=head->next; head2=head2->next;
+        }
+        h=h->next;
+        return h;
+    }
+    ListNode* marge(ListNode* head){
+        //cout<<head->val<<endl;
+        if(head == NULL || head->next == NULL){return head;}
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* pre;
+        while(fast!=NULL && fast->next!=NULL){
+            pre=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* first = head;
+        ListNode* second = slow;
+        pre->next=NULL;
+        // cout<<slow->val<<endl;
+        first=marge(first);
+        second=marge(second);
+        
+        ListNode* final = mergeTwoLists(first,second);
+        return final;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        return marge(head);
+    }
+};
