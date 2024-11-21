976fe29df4121b61f3e30f0b77b1669bbf885ea0
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-11-21 12:19:42 +0600

    Time: 13 ms (46.91%) | Memory: 19.5 MB (7.98%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-        map<int,int>hash;
+        map<int,vector<int>>mp;
         for(int i=0;i<nums.size();i++){
-            hash[nums[i]]=i+1;
+            mp[nums[i]].push_back(i);
         }
+        vector<int>ans;
+
         for(int i=0;i<nums.size();i++){
-            int remain = target - nums[i];
-            if(hash[remain] != 0 && (hash[remain]-1) != i){
-                v.push_back(i);
-                v.push_back(hash[remain]-1);break;
+            int remain = target-nums[i];
+
+            if(mp[remain].size() > 0){
+                int indexi=i,indexj;
+                vector<int>v=mp[remain];
+                int count=0;
+                for(int j=0;j<v.size();j++){
+                    if(i!=v[j]){
+                        indexj=v[j];count=1;
+                        ans.push_back(indexi),ans.push_back(indexj);
+                        break;
+                    }
+                }
+                if(count==1){break;}
             }
         }
-        return v;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
