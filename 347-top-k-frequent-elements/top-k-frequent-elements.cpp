e7553fedc84e31e8ba833dbf30a4f1d7252f24a8
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-11-22 20:49:12 +0600

    Time: 11 ms (8.01%) | Memory: 16.9 MB (86.97%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,23 +2,32 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int,int>mp;
-        vector<int>ans;
+        map<int,vector<int>>mp2;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        map<int,vector<int>>vi;
-        map<int,vector<int>>::reverse_iterator it;
-        for(auto a=mp.begin();a!=mp.end();a++){
-            vi[a->second].push_back(a->first);
-        }
-        for(it=vi.rbegin();it!=vi.rend();it++){
-            for(int i=0;i<it->second.size();i++){
-                ans.push_back(it->second[i]);
-                if(ans.size()==k){break;}
-            }
-            if(ans.size()==k){break;}
+
+        for(auto it=mp.begin();it!=mp.end();it++){
+            mp2[it->second].push_back(it->first);
         }
 
+        vector<int>ans;
+
+        for(auto it=mp2.rbegin();it!=mp2.rend();it++){
+            vector<int>v=it->second;
+            for(int i=0;i<v.size();i++){
+                if(k>0){
+                    ans.push_back(v[i]);
+                    k--;
+                }
+                else{
+                    break;
+                }
+            }
+            if(k<=0){break;}
+        }
+        
+        
         return ans;
     }
 };
