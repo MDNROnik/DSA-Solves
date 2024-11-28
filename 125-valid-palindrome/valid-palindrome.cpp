0a9e19efec521728847586987509dfb40d4f1a6e
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-11-28 12:56:02 +0600

    Time: 448 ms (7.35%) | Memory: 9.7 MB (10.83%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str,str2;
-        for(int i=0;i<s.size();i++){
-            if('a'<=s[i] && s[i]<='z'){
-                str.push_back(s[i]);
-                str2.push_back(s[i]);
+        int i=0,j=s.size()-1;
+        int c=1;
+        while(i<j){
+            cout<<s[i]<<" "<<s[j]<<endl;
+            if( ((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9') ) &&
+                ((s[j]>='a' && s[j]<='z') || (s[j]>='A' && s[j]<='Z') || (s[j]>='0' && s[j]<='9') )
+            ){
+                char a=(char)tolower(s[i]);
+                char b=(char)tolower(s[j]);
+                cout<<a<<" IN "<<b<<endl;
+                if(a!=b){
+                    c=0;
+                    break;
+                }
+                i++;j--;
             }
-            else if('A'<=s[i] && s[i]<='Z'){
-                str.push_back(tolower(s[i]));
-                str2.push_back(tolower(s[i]));
-            }
-            else if('0'<=s[i] && s[i]<='9'){
-                str.push_back(s[i]);
-                str2.push_back(s[i]);
+            else{
+                
+                if( ((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9') )  ){
+                    j--;
+                }
+                else if( ((s[j]>='a' && s[j]<='z') || (s[j]>='A' && s[j]<='Z') || (s[j]>='0' && s[j]<='9') ) ){
+                    i++;
+                }
+                else{
+                    i++;j--;
+                }
+                
             }
         }
-        reverse(str2.begin(),str2.end());
-        cout<<str<<" "<<str2<<endl;
-        if(str==str2){
-            return true;
-        }
-        else{
+        if(c==0){
             return false;
         }
+        return true;
     }
 };
