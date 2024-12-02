af989766948388331ceff625b6649edff7c4000b
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-12-02 23:41:09 +0600

    Time: 9 ms (100.00%) | Memory: 12.4 MB (5.14%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m>0 && n==0){return;}
-        else if(m==0 && n>0){
-            nums1=nums2;return;
-        }
         vector<int>vec;
-        int i=0;int j=0;
-        int temp=nums2[n-1];
+        int i=0,j=0;
+        //cout<<i<<" "<<(m-n)<<" "<<j<<" "<<n<<endl;
         while(i<m && j<n){
-            if(nums1[i]<nums2[j] ){
+            cout<<nums1[i]<<" "<<nums2[j]<<endl;
+            if(nums1[i]<=nums2[j]){
                 vec.push_back(nums1[i]);i++;
             }
-            else if(nums1[i]>nums2[j]){
-                vec.push_back(nums2[j]);j++;
-            }
-            else if(nums1[i]==nums2[j]){
-                vec.push_back(nums1[i]);i++;
+            else{
                 vec.push_back(nums2[j]);j++;
             }
         }
-        if(j<n){
-            for(;j<n;j++){
-                vec.push_back(nums2[j]);
-            } 
+        while(i<m){
+            vec.push_back(nums1[i]);i++;
         }
-        else if(i<m){
-            for(;i<m;i++){
-                vec.push_back(nums1[i]);
-            } 
+        while(j<n){
+            vec.push_back(nums2[j]);j++;
         }
-        
+        cout<<vec.size()<<endl;
         nums1=vec;
     }
 };
