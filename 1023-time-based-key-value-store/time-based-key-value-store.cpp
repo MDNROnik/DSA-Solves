3288f939238ba404bce04c41196d7f223c415d03
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-12-14 11:57:19 +0600

    Time: 55 ms (69.10%) | Memory: 136.9 MB (24.02%) - LeetSync

---
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>>mp;
+public:
+    TimeMap() { }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].emplace_back(timestamp,value);
+    }
+    
+    string get(string key, int timestamp) {
+        auto& v = mp[key] ;
+        int r=v.size()-1;
+        int l=0;
+        //sort(v.begin(),v.end());
+        string ans = "";
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(v[mid].first==timestamp){
+                return v[mid].second;
+            }
+            else if(v[mid].first>timestamp){
+                r=mid-1;
+            }
+            else{
+                ans=v[mid].second;
+                l=mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// class TimeMap {
+// private:
+//     unordered_map<string, vector<pair<int, string>>> keyStore;
+
+// public:
+//     TimeMap() {}
+
+//     void set(string key, string value, int timestamp) {
+//         keyStore[key].emplace_back(timestamp, value);
+//     }
+
+//     string get(string key, int timestamp) {
+//         auto& values = keyStore[key];
+//         int left = 0, right = values.size() - 1;
+//         string result = "";
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (values[mid].first <= timestamp) {
+//                 result = values[mid].second;
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
