93da8ed8ae18974bab42b1bc1667d99a3c2ba425
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2024-12-24 12:59:35 +0600

    Time: 145 ms (15.76%) | Memory: 54.7 MB (69.68%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    Solution() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+    }
     bool p(string &temp){
         string str = temp;
         reverse(str.begin(),str.end());
