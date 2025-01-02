facdb73c068c7cf9c0ec09cad95c07cb7ef24c49
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-02 11:55:19 +0600

    Time: 0 ms (100.00%) | Memory: 10.8 MB (14.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
-int solveUtil(int ind, vector<int>& arr, vector<int>& dp) {
-    if (dp[ind] != -1)
-        return dp[ind];
-
-    if (ind == 0) 
-        return arr[ind];
-    if (ind < 0)  
-        return 0;
-
-    int pick = arr[ind] + solveUtil(ind - 2, arr, dp); 
-    int nonPick = 0 + solveUtil(ind - 1, arr, dp);    
-
-    return dp[ind] = max(pick, nonPick);
-}
-int solveUtil2(int n, vector<int>& arr, vector<int>& dp) {
-    dp[0] = arr[0];
-    
-    for (int i = 1; i < n; i++) {
-        int pick = arr[i];
-        if (i > 1)
-            pick += dp[i - 2];
-        int nonPick = dp[i - 1];
-        
-        dp[i] = max(pick, nonPick);
-    }
-    
-    return dp[n - 1];
-}
-    int rob(vector<int>& arr) {
-        int prev = arr[0];   
-        int prev2 = 0;       
-        
-        for (int i = 1; i < arr.size(); i++) {
-            int pick = arr[i];  
-            if (i > 1)
-                pick += prev2;  
-            
-            int nonPick = 0 + prev;  
-            
-            int cur_i = max(pick, nonPick);  
-            prev2 = prev;  
-            prev = cur_i;
+    int fun( vector<int> &mp, int i, int n, vector<int> &nums ){
+        if(i>=n){
+            return 0;
         }
+        if(mp[i]>-1){
+            return mp[i];
+        }
+        int notpick = fun(mp, i+1, n, nums);
+        int pick = nums[i] + fun(mp, i+2, n, nums);
+
+        return mp[i] = max(notpick, pick);
+    }
+    int rob(vector<int>& nums) {
         
-        return prev; 
+        int n=nums.size();
+        vector<int>mp(n,-1);
+        return fun(mp, 0, n, nums);
     }
 };
