dc9a95460ace1fa097a68126cde37e27d3c25bf5
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-03 11:25:59 +0600

    Time: 78 ms (9.59%) | Memory: 50.1 MB (17.39%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int fun(map<pair<int,int>  , int> &mp, vector<int> &coins, int amount, int index){
+    int fun(vector<vector<int>> &mp, vector<int> &coins, int amount, int index){
+        
         //cout<<index<<" "<<amount<<endl;
         if(amount==0){
             return 0;
         }
         if(index>=coins.size() || amount<0){
-            return 214748364;
+            return 1e9;
         }
-        if(mp[{index, amount}]>0){
-            return mp[{index, amount}];
+        if(mp[index][amount]!= -1){
+            return mp[index][amount];
         }
 
         // int time = amount / coins[index];
@@ -19,14 +20,17 @@ public:
         int pickandstay = 1 + fun(mp, coins, amount-coins[index], index);
         int pickandforward = 1 + fun(mp, coins, amount-coins[index], index+1);
 
-        return mp[{index, amount}] = min( notpick, min(pickandstay,pickandforward) );
+        // cout<<notpick<<" "<<pickandstay<<" "<<pickandforward<<endl;
+        return mp[index][amount] = min( notpick, min(pickandstay,pickandforward) );
     }
     int coinChange(vector<int>& coins, int amount) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-        map<pair<int,int> , int>mp;
+        // map<pair<int,int> , int>mp;
+        int n = coins.size();
+        vector<vector<int>>mp(n, vector<int>(amount+1, -1));
         int ans =  fun(mp, coins, amount, 0);
-        if(ans == 214748364){
+        if(ans >= 1000000000 ){
             return -1;
         }
         return ans;
