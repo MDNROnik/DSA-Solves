0e26bded87b3c4f86c9095db2a709f0eee961f9a
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-06 23:32:13 +0600

    Time: 2 ms (33.81%) | Memory: 11.8 MB (6.52%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fun(int now, vector<int>&mp){
         if(now == 0 || now == 1){
-            return now;
+            return mp[now]=now;
         }
         if(mp[now]!=0){
             return mp[now];
@@ -13,10 +13,10 @@ public:
     }
     vector<int> countBits(int n) {
         vector<int>ans;
-        vector<int>mp(n+10, 0);
+        vector<int>mp(n+1, 0);
         for(int i=0;i<=n;i++){
             ans.push_back( fun(i, mp) );
         }
-        return ans;
+        return mp;
     }
 };
