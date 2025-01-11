16bb8373ea5777b4a46cba0c920e75d64b4bba4b
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-11 12:34:24 +0600

    Time: 0 ms (100.00%) | Memory: 20.4 MB (50.70%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -4,11 +4,11 @@ public:
         int init = 0, n=nums.size(), ans = 0;
         for(int i=0;i<n;i++){
             int j;
-            cout<<"START "<<init<<" "<<i<<endl;
+            //cout<<"START "<<init<<" "<<i<<endl;
             int past = init;
             for(j=i;j<=past && j<n; j++){
                 int step = nums[j]+j;
-                cout<<j<<" "<<nums[j]<<endl;
+                //cout<<j<<" "<<nums[j]<<endl;
                 if(init < step){
                     init=step;
                 }
