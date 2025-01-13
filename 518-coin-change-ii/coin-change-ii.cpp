1dd3d9e90666ad24e38c986cee39d49327816094
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-13 18:19:52 +0600

    Time: 38 ms (18.37%) | Memory: 31 MB (26.41%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int fun(vector<vector<int>> &mp, vector<int> &coins, int amount, int index, int n){
-        
+    int fun(vector<vector<int>> &v, int amount, int index, vector<int>& coins, int &n){
         //cout<<index<<" "<<amount<<endl;
         if(amount==0){
             return 1;
         }
-        if(index>=n || amount<0){
+        else if(amount<0){
             return 0;
         }
-        if(mp[index][amount]!= -1){
-            return mp[index][amount];
+        if(index>=n){
+            return 0;
         }
+        if(v[index][amount] != -1){
+            return v[index][amount];
+        }
+        int take = fun(v, amount-coins[index], index, coins, n);
+        int not_take = fun(v, amount, index+1, coins, n);
 
-        // int time = amount / coins[index];
-        // int remain = amount % coins[index];
-
-        int notpick = fun(mp, coins, amount, index+1, n);
-        int pickandstay =  fun(mp, coins, amount-coins[index], index, n);
-        // int pickandforward = 1 + fun(mp, coins, amount-coins[index], index+1, n);
-
-        int pickandforward = 0;
-        // cout<<notpick<<" "<<pickandstay<<" "<<pickandforward<<endl;
-        return mp[index][amount] = notpick+ pickandstay;
+        return v[index][amount] = (take + not_take);
     }
     int change(int amount, vector<int>& coins) {
         ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        // map<pair<int,int> , int>mp;
+        cin.tie(NULL);
         int n = coins.size();
-        vector<vector<int>>mp(n, vector<int>(amount+1, -1));
-        int ans =  fun(mp, coins, amount, 0, n);
-        
-        return ans;
+        vector<vector<int>>v(n+10, vector<int>(amount+10, -1));
+        return fun(v, amount, 0, coins, n);
     }
 };
