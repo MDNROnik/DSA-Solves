38c5710aa1b7c2313602a6ade91677b1722e83e2
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-14 11:31:03 +0600

    Time: 287 ms (39.78%) | Memory: 62.6 MB (8.30%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fun(map<pair<int,int>, bool> &visited, map<pair<int,int>, int> &value, int index, int total,int n, int target,
+    int fun(map<pair<int,int>, pair<bool,int>>&visited, int index, int total,int n, int target,
     vector<int>& nums){
         if(index>=n){
             if(total==target){
@@ -10,26 +10,25 @@ public:
                 return 0;
             }
         }
-        if(visited[{index, total}]==true){
-            return value[{index, total}];
+        if(visited[{index, total}].first==true){
+            return visited[{index, total}].second;
         }
         int now = total + nums[index];
         int now2= total + (nums[index]*(-1));
 
-        int positive = fun(visited, value, index+1, now, n, target, nums);
+        int positive = fun(visited, index+1, now, n, target, nums);
 
-        int negative = fun(visited, value, index+1, now2, n, target, nums);
+        int negative = fun(visited, index+1, now2, n, target, nums);
 
-        visited[{index, total}]=true;
+        visited[{index, total}].first=true;
 
         int total2 = positive + negative;
-        return value[{index, total}] = total2;
+        return visited[{index, total}].second = total2;
 
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        map<pair<int,int>, bool> visited;
-        map<pair<int,int>, int> value;
+        map<pair<int,int>, pair<bool,int>> visited;
         int n = nums.size();
-        return fun(visited, value, 0, 0, n, target, nums);
+        return fun(visited, 0, 0, n, target, nums);
     }
 };
