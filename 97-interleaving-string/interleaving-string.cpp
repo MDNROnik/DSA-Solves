cff105aef6920ceaf13a491c3477e15860c57a22
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-15 13:11:08 +0600

    Time: 3 ms (60.72%) | Memory: 10 MB (29.50%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fun(string &s1, string &s2, string &s3, string &s4 , vector< vector<int> >&vp, int i, int j, int k, int &n, int &m){
+    int fun(string &s1, string &s2, string &s3, vector< vector<int> >&vp, int i, int j, int k, int &n, int &m){
         
         if(i==n && j==m ){
             return 1;
@@ -14,18 +14,18 @@ public:
         int ans = 0;
         if(s1[i]==s3[k] && s2[j]==s3[k]){
             //cout<<3<<endl;
-            int ans1= fun(s1, s2, s3, s4, vp, i+1, j, k+1, n, m);
-            int ans2= fun(s1, s2, s3, s4, vp, i, j+1, k+1, n, m);
+            int ans1= fun(s1, s2, s3, vp, i+1, j, k+1, n, m);
+            int ans2= fun(s1, s2, s3, vp, i, j+1, k+1, n, m);
             return vp[i][j] = ans1 || ans2;
         }
         else if(s1[i]==s3[k] && i<n){
             //cout<<1<<endl;
-            int ans = fun(s1, s2, s3, s4, vp, i+1, j, k+1, n, m);
+            int ans = fun(s1, s2, s3, vp, i+1, j, k+1, n, m);
             return vp[i][j]=ans;
         }
         else if(s2[j]==s3[k] && j<m){
             //cout<<2<<endl;
-            int ans = fun(s1, s2, s3, s4, vp, i, j+1, k+1, n, m);
+            int ans = fun(s1, s2, s3, vp, i, j+1, k+1, n, m);
             return vp[i][j]=ans;
         }
         else{
@@ -50,8 +50,7 @@ public:
             return false;
         }
         vector< vector<int> >vp(n+10, vector<int> (m+10, -1));
-        string s4="";
 
-        return fun(s1, s2, s3, s4, vp, 0, 0, 0, n, m);
+        return fun(s1, s2, s3, vp, 0, 0, 0, n, m);
     }
 };
