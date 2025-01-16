5cfce283d616b00375eeaf9d8e799d1fbc7ef8ef
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-16 12:03:14 +0600

    Time: 146 ms (5.11%) | Memory: 86.5 MB (5.31%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fun(string &word1, int i, string &word2, int j, vector<vector<vector<int>>> &v, int &n, int &m, int mode){
+    int fun(string &word1, int i, string &word2, int j, vector<vector<vector<int>>> &v, int &n, int &m, int mode,  vector<vector<int>> &vp){
        
         if(i==n && j==m){
             return 0;      
@@ -11,26 +11,31 @@ public:
         else if(i<n && j==m){
             return (n-i);
         }
-        if(v[i][j][mode] != -1){
-            return v[i][j][mode];
+        if(vp[i][j]!=-1){
+            return vp[i][j];
         }
+        // if(v[i][j][mode] != -1){
+        //     return v[i][j][mode];
+        // }
         int match=10000, remove=10000, replace=10000, insert=10000;
         if(word1[i]==word2[j]){
-            match = fun(word1, i+1, word2, j+1, v, n, m, 1);
+            match = fun(word1, i+1, word2, j+1, v, n, m, 1, vp);
         }
         else{
-            remove = 1 + fun(word1, i+1, word2, j, v, n, m, 2);
-            replace = 1 + fun(word1, i+1, word2, j+1, v, n, m, 3);
-            insert = 1 + fun(word1, i, word2, j+1, v, n, m, 4);
+            remove = 1 + fun(word1, i+1, word2, j, v, n, m, 2, vp);
+            replace = 1 + fun(word1, i+1, word2, j+1, v, n, m, 3, vp);
+            insert = 1 + fun(word1, i, word2, j+1, v, n, m, 4, vp);
         }
-        return v[i][j][mode] = min(match, min(remove, min(replace, insert)));
+        // return v[i][j][mode] = min(match, min(remove, min(replace, insert)));
+
+        return vp[i][j] = min(match, min(remove, min(replace, insert)));
     }
     int minDistance(string word1, string word2) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n=word1.size(), m=word2.size();
         vector<vector<vector<int>>>v(n+10,vector<vector<int>>(m+10, vector<int>(10, -1)));
-
-        return fun(word1, 0, word2, 0, v, n, m, 0);
+        vector<vector<int>>vp(n+10,vector<int>(m+10,-1));
+        return fun(word1, 0, word2, 0, v, n, m, 0, vp);
     }
 };
