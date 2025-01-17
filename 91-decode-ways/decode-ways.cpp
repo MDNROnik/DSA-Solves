00763292580cd4babf821e18f6e33ecc05f95161
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-17 10:20:06 +0600

    Time: 2 ms (28.01%) | Memory: 8.6 MB (68.04%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -10,21 +10,17 @@ public:
         if(s[i] == '0'){
             return v[i]=0;
         }
-        
         int oneStep = 0, twoStep = 0;
-        cout<<"step 1 " <<s[i]<<endl;
         oneStep = fun(v, n, s, i+1);
         int now = s[i]-'0';
         now*=10;
         if(i+1<n){
             now+=s[i+1]-'0';
-            //cout<<now<<endl;
             if(now>0 && now<27){
                 cout<<"step 2 " <<s[i]<<s[i+1]<<endl;
                 twoStep = fun(v, n, s, i+2);
             }
         }
-        //cout<<s[i]<<" "<<oneStep<<" "<<twoStep<<endl;
         return v[i] = oneStep+twoStep;
     }
     int numDecodings(string s) {
