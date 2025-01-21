d21fae074a39bdc9da10c5044f88c44b30cd1228
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-21 12:50:07 +0600

    Time: 9 ms (3.90%) | Memory: 8.5 MB (66.74%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
+
+    double fun(double x, long n){
+        if(n==1){
+            return x;
+        }
+        double now = fun(x, n/2);
+
+        if(n%2 == 0){
+            cout<<"even"<<endl;
+            double res = now*now;
+            cout<<n<<" "<<now<<" "<<res<<endl;;
+            return res;
+        }
+        else{
+            cout<<"ODD"<<endl;
+            double res = now*now;
+            res*=x;
+            cout<<n<<" "<<now<<" "<<res<<endl;;
+            return res;
+        }
+    }
     double myPow(double x, int n) {
-        double ans = pow(x, n);
-        return ans;
+        if(n==0){
+            return 1;
+        }
+        double res = fun(x, abs(static_cast<long>(n)));
+        if(n>=0){
+            return res;
+        }
+        else{
+            return 1/res;
+        }
     }
 };
