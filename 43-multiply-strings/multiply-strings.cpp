969828f2a3dc7fad5a73dacbd91147bd8d809bae
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-22 16:05:39 +0600

    Time: 33 ms (8.94%) | Memory: 15.1 MB (12.63%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        vector<string>vs;
+vector<string>vs;
         int gap = 0;
-        if(num1=="0" || num2=="0"){
+        int mi=0;
+
+        while(num1[0]=='0' || num1[0]=='-'){
+            if(num1[0]=='-'){
+                mi++;
+            }
+            num1.erase(num1.begin() + 0);
+        }
+        while(num2[0]=='0' || num2[0]=='-'){
+            if( num2[0]=='-'){
+                mi++;
+            }
+            num2.erase(num2.begin() + 0);
+        }
+        if(num1.size()==0 || num2.size()==0 ){
             return "0";
         }
-        int siz = num1.size()>=num2.size() ? num1.size()+10 : num2.size()+10;
-        for(int i=num2.size()-1;i>=0;i--){
+        int n=num1.size(), m=num2.size();
+        int siz = n>=m ? n+10 : m+10;
+        for(int i=m-1;i>=0;i--){
             string now ;
             for(int j=0; j<gap ; j++) {
                 now.push_back('0');
             }
             int carry = 0;
-            for(int j=num1.size()-1;j>=0;j--){
+            for(int j=n-1;j>=0;j--){
                 int x= num2[i]-'0';
                 int y= num1[j]-'0';
                 int z = x*y;
@@ -34,13 +49,11 @@ public:
             vs.push_back(now);
         }
         string ans;
-        // for(int i=0;i<vs.size();i++){
-        //     cout<<vs[i]<<endl;
-        // }
-        int carry = 0;
-        for(int j=0;j<vs[0].size();j++){
+
+        int carry = 0 , o = vs.size(), p=vs[0].size();
+        for(int j=0;j<p;j++){
             int now=0;  
-            for(int i=0;i<vs.size();i++){
+            for(int i=0;i<o;i++){
                 int temp=(vs[i][j]-'0');
                 now+=temp;
             }
@@ -60,8 +73,11 @@ public:
             ans.pop_back();
             s=ans.size()-1;
         }
+        if(mi==1){
+            ans.push_back('-');
+        }
         reverse(ans.begin(),ans.end());
-        int n = 7;
+
         return ans;
     }
 };
