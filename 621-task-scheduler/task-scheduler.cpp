fbfdfdbe0a1f7cd0f549c8b8a0d229df3f079d6a
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-27 13:22:11 +0600

    Time: 29 ms (45.93%) | Memory: 46.7 MB (40.83%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,50 +1,13 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int m) {
-        vector<int>v(30, 0);
+        vector<int>v(26, 0);
         int n=tasks.size();
         for(int i=0;i<tasks.size();i++){
             int ind = tasks[i];
             ind-=65;
             v[ind]++;
         }
-        // if(m==1){
-        //     vector<int>count;
-        //     for(int i=0;i<30;i++){
-        //         if(v[i]>0){
-        //             count.push_back(v[i]);
-        //         }
-        //     }
-        //     sort(count.begin(),count.end());
-        //     int id=0, ans=0, l=0, r=count.size()-1;
-
-        //     while(n>0){
-        //         int loop=0;
-        //         while(count[l]==0){
-        //             l++;
-        //         }
-        //         while(count[r]==0){
-        //             r--;
-        //         }
-        //         while(l<=r){
-        //             if(id<=m && id!=0){
-        //                 break;
-        //             }
-        //             id++;n--;count[l]--;ans++;
-        //             if(count[l]==0){l++;}
-        //             if(l>=r){break;}
-        //             id++,n--;count[r]--;ans++;
-        //             if(count[r]==0){r--;}
-        //         }
-        //         if( ((id<=m && count.size()==1) || id<m) && n>0){
-        //             ans+=(m-id)+1;
-        //         }
-        //         id=0;
-        //         l=0;
-        //         r=count.size()-1;
-        //     }
-        //     return ans;
-        // }
         vector<int>count;
         for(int i=0;i<26;i++){
             if(v[i]>0){
