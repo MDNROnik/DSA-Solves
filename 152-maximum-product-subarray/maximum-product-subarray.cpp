0555599ba08035e7344e2fe4ac56bacf6a424770
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-01-28 12:35:24 +0600

    Time: 0 ms (100.00%) | Memory: 19.7 MB (5.62%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    
-    int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int mul=0, ans = INT_MIN;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                ans = max(ans, 0);
-                mul=0; 
-            }
-            else{
-                ans = max(ans, nums[i]);
-                if(mul==0){
-                    mul=nums[i];
-                }
-                else{
-                    cout<<mul<<endl;
-                    mul*=nums[i];
-                    cout<<mul<<endl;
-                }
-                ans = max(ans, mul);
-            }
+    pair<int,int> fun(vector< pair<int,int> >&mp, vector<int>&nums, int i, int &n){
+        if(i>=n){
+            return {1,1};
         }
-        mul=0;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]==0){
-                ans = max(ans, 0);
-                mul=0; 
-            }
-            else{
-                ans = max(ans, nums[i]);
-                if(mul==0){
-                    mul=nums[i];
-                }
-                else{
-                    mul*=nums[i];
-                }
-                ans = max(ans, mul);
-            }
+        if(mp[i].first!=INT_MAX){
+            return mp[i];
+        }
+        pair<int,int> now = fun(mp, nums, i+1, n);
+        int m = now.first;
+        int m2 = now.second;
+        int maxi = now.first*nums[i];
+        int mini = now.second*nums[i];
+        nums[i]*=1;
+        int first = max(maxi, max(nums[i], mini));
+        int second = min(maxi, min(nums[i], mini));
+
+        // cout<<nums[i]<<" "<<first<<" "<<second<<endl;
+        return mp[i] = {first, second};
+    }
+    int maxProduct(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n = nums.size();
+        vector< pair<int,int> >mp(n+10, {INT_MAX, INT_MAX});
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            pair<int,int> p = fun(mp, nums, i, n);
+            ans = max(ans, max(p.first, p.second));
         }
         return ans;
     }
