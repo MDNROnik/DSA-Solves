851d7167474e723dd50b901e75103eee4932ccf5
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-02 10:42:10 +0600

    Time: 169 ms (5.04%) | Memory: 110.6 MB (5.01%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int check(string &s, int &n, int i, int l, int r, map<pair<int, stack<char>>, int> &mp, stack<char> &str){
+        // if(l<r){
+        //     return 1;
+        // }
+        if(n==i){
+            if(l==r){
+                return 2;
+            }
+            else{
+                return 1;
+            }
+        }
+        if(mp[{i, str}]!=0){
+            return mp[{i, str}];
+        }
+        if(s[i]=='('){
+            str.push('(');
+            int ans = check(s, n, i+1, l+1, r, mp, str);
+            if(ans==2){return mp[{i, str}]= 2;}
+            str.pop();
+        }
+        if(s[i]==')' && str.size()>0 && str.top()=='('){
+            str.pop();
+            int ans = check(s, n, i+1, l, r+1, mp, str);
+            if(ans==2){return mp[{i, str}]= 2;}
+            str.push('(');
+        }
+        if(s[i]=='*'){
+            int ans = check(s, n, i+1, l, r, mp, str);
+            if(ans==2){return mp[{i, str}]= 2;}
+        }
+        if(s[i]=='*'){
+            str.push('(');
+            int ans = check(s, n, i+1, l+1, r, mp, str);
+            if(ans==2){return mp[{i, str}]= 2;}
+            str.pop();
+        }
+        if(s[i]=='*' && str.size()>0 && str.top()=='('){
+            str.pop();
+            int ans = check(s, n, i+1, l, r+1, mp, str);
+            if(ans==2){return mp[{i, str}]= 2;}
+            str.push('(');
+        }
+        
+        return mp[{i, str}]=1;
+    }
+    bool checkValidString(string s) {
+        int n=s.size();
+        map<pair<int, stack<char>>, int>mp;
+        stack<char> str;
+        if( check(s, n, 0, 0, 0, mp, str) == 2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
