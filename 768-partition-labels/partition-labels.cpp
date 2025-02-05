9007c929bef7010afe988a4ed905549c150f457f
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-05 11:08:37 +0600

    Time: 4 ms (13.93%) | Memory: 8.8 MB (89.34%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,68 +1,29 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        map<char,pair<int,pair<int,int>>>mp;
+        vector<int>v(30,-1);
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            int index = s[i];
+            index-=97;
+            v[index] = i; 
+        }
         vector<int>ans;
-
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            //cout<<mp[s[i]].second.first<<endl;
-            if(mp[s[i]].first == 0){
-                //cout<<s[i]<<endl;
-                mp[s[i]].first = 1;
-                mp[s[i]].second={i,i};
+        for(int i=0;i<n;){
+            int j = i;
+            int maxIndex=i;
+            int count=0;
+            while(j<=maxIndex && j<n){
+                int index = s[j];
+                index-=97;
+                maxIndex= max(maxIndex, v[index]);
+                count++;
+                j++;
             }
-            else{
-                mp[s[i]].second.second=i;
-            }
+            cout<<count<<" "<<j<<endl;
+            i=j;
+            ans.push_back(count);
         }
-        // vector< pair<int,int> >v;
-        // for(auto a=mp.begin();a!=mp.end();a++){
-        //     //cout<<a->first<<" "<<a->second.second.first<<"  "<<a->second.second.second<<endl;
-        //     v.push_back({ a->second.second.second , a->second.second.first });
-        // }
-        // sort(v.begin(),v.end());
-        // n=v.size();
-        // int remain=0;
-        // for(int i=0;i<n;i++){
-        //     remain=v[i].first;
-        //     for(int j=i;j<n;j++){
-        //         if(v[j].first >= v[j].second){
-
-        //         }
-        //     }
-        // }
-
-        int low = mp[s[0]].second.first;
-        int high = mp[s[0]].second.second;
-        int count=0;
-        for(int i=0;i<n;i++){
-            count+=1;
-            int l = mp[s[i]].second.first;
-            int h = mp[s[i]].second.second;
-            cout<<i<<endl;
-            cout<<low<<" "<<high<<" "<<count<<endl;
-            cout<<s[i]<<" "<<l<<" "<<h<<endl<<endl;;
-            if(high==h && count==(high-low+1)){
-                ans.push_back(high-low+1);
-                if(i+1<n){
-                    low = mp[s[i+1]].second.first;
-                    high = mp[s[i+1]].second.second;
-                    count=0;
-                }
-                else{
-                    break;
-                }
-            }
-            else if(high<h){
-                high=h;
-            }
-            else{
-                continue;
-            }
-        }
-
-
         return ans;
     }
 };
