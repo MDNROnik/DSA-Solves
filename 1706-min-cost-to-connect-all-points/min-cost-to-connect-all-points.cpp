35c2ab788a65ad6cca3858204ee910606490141a
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-08 12:17:09 +0600

    Time: 275 ms (57.91%) | Memory: 18.4 MB (86.80%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
-        // sort(points.begin(),points.end());
         map<pair<int,int>,pair<int, int>>mp;
+
         priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> gq;
         gq.push({0, n-1});
-        // mp[{points[n-1][0], points[n-1][1]}].first=true;
         mp[{points[n-1][0], points[n-1][1]}].second = 0;
         long long a=0;
         while(!gq.empty()){
@@ -17,40 +16,28 @@ public:
             int col = points[index][1];
             if(mp[{row, col}].first==2 || mp[{row, col}].second<dis){continue;}
             mp[{row, col}].first=2;
-            // cout<<dis<<endl;
             a+=dis;
             int c=0;
-            cout<<row<<" "<<col<<" "<<dis<<endl;
             for(int i=0;i<n;i++){
                 int row2 = points[i][0];
                 int col2 = points[i][1];
                 if(mp[{row2, col2}].first==2){
                     continue;
                 }
-                // cout<<row2<<" "<<col2<<endl;
                 int sum = abs(row-row2) + abs(col-col2);
                 if(mp[{row2, col2}].first==0){
                     mp[{row2, col2}].first=1;
                     mp[{row2, col2}].second=INT_MAX;
                 }
-                // cout<<mp[{row2, col2}].second<<" "<<sum<<endl<<endl;;
                 if( mp[{row2, col2}].second > sum){
                     mp[{row2, col2}].second=sum;
                     gq.push({sum, i});
                     c++;
                 }
             }
-            // if(c>0){
-            //     // cout<<row<<" "<<col<<endl; 
-            //     a+=dis;
-            // }
+
         }
         int ans = 0;
-        // for(auto a=mp.begin();a!=mp.end();a++){
-        //     // cout<<a->first.first<<" "<<a->first.second<<" --- "<<a->second.first<<" "<<a->second.second<<endl;
-        //     ans+=a->second.second;
-        // }
-        cout<<a<<endl;
         return a;
     }
 };
