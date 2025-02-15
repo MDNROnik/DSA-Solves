f3710a2a6dc587f43f5fef11d1bd708334bafa41
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-15 15:10:16 +0600

    Time: 335 ms (21.57%) | Memory: 268.8 MB (5.00%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<vector<int>>vec;
+        vector<int>v(26, 0);
+        int n = s.size();
+        vector<vector<int>>index(26, vector<int>(2, -1));
+        for(int i=0;i<n;i++){
+            int now = (int)s[i];
+            now-=97;
+            v[now]++;
+            vec.push_back(v);
+            if(index[now][0]==-1){
+                index[now][0]=i;
+            }
+            index[now][1]=i;
+        }
+        int ans=0;
+        for(int i=0;i<26;i++){
+            int left = index[i][0];
+            int right = index[i][1];
+            if(left==-1){continue;}
+            // cout<<left<<" "<<right<<endl;
+            int count=0;
+            // for(int j=0;j<26;j++){
+            //     cout<<vec[left][j]<<" ";
+            // }
+            // cout<<endl;
+            // for(int j=0;j<26;j++){
+            //     cout<<vec[right][j]<<" ";
+            // }
+            // cout<<endl;
+            for(int j=0;j<26;j++){
+                if( (vec[right][j]-vec[left][j]) >=1  && vec[right][j]==2 && j==i){
+                    cout<<111<<endl;
+                }
+                else if( (vec[right][j]-vec[left][j]) >=1 ){
+                    cout<<222<<endl;
+                    count++;
+                }
+            }
+            // cout<<count<<endl;
+            ans+=count;
+            // cout<<endl;
+            // for(int j=0;j<26;j++){
+            //     cout<<vec[right][j]<<" ";
+            // }
+            // cout<<endl;
+        }
+        return ans;
+    }
+};
