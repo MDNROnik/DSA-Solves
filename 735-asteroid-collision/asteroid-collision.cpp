40f6be06a94bb0083a77a5e2ec859ef0bcd079c7
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-17 10:03:23 +0600

    Time: 8 ms (5.77%) | Memory: 22.8 MB (6.93%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& as) {
+        vector<int>ans;
+        int n=as.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(st.size()==0 ){
+                st.push(as[i]);
+            }
+            else if(st.top()<0 && as[i]<0){
+                 st.push(as[i]);
+            }
+            else if(st.top()>=0 && as[i]>=0){
+                 st.push(as[i]);
+            }
+            else{
+                st.push(as[i]);
+                while(st.size()>=2){
+                    int first = st.top();st.pop();
+                    int second = st.top();st.pop();
+                    if( (first<0 && second>=0) ){
+                        int firstSize = abs(first);
+                        int secondSize = abs(second);
+                        if(firstSize==secondSize){
+
+                        }
+                        else if(firstSize>secondSize){
+                            st.push(first);
+                        }
+                        else{
+                            st.push(second);
+                        }
+                    }
+                    else{
+                        st.push(second), st.push(first);break;
+                    }
+                }
+            }
+        }
+        while(st.size()>0){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
