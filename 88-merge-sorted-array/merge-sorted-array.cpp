4f123a67510f1e3859638a5afc0b0a772d4fec16
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-24 10:14:42 +0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (39.60%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         int i=0,j=0;
         //cout<<i<<" "<<(m-n)<<" "<<j<<" "<<n<<endl;
         while(i<m && j<n){
-            cout<<nums1[i]<<" "<<nums2[j]<<endl;
+            // cout<<nums1[i]<<" "<<nums2[j]<<endl;
             if(nums1[i]<=nums2[j]){
                 vec.push_back(nums1[i]);i++;
             }
@@ -19,7 +19,7 @@ public:
         while(j<n){
             vec.push_back(nums2[j]);j++;
         }
-        cout<<vec.size()<<endl;
+        // cout<<vec.size()<<endl;
         nums1=vec;
     }
 };
