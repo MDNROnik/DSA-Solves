28941cb799b592889fa15ea2a75df6825550c5d3
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-24 13:34:06 +0600

    Time: 2 ms (26.21%) | Memory: 9.6 MB (42.33%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -6,12 +6,12 @@ public:
         }
         string digit = "";
         while(i<n && s[i]>='0' && s[i]<='9'){
-            digit.push_back(s[i]);i++;
+            digit+=(s[i]);i++;
         }
         if(s[i]=='['){i++;}
         string str="";
         while(i<n && s[i]>='a' && s[i]<='z'){
-            str.push_back(s[i]);i++;
+            str+=(s[i]);i++;
         }
         if(s[i]==']'){
             if(digit.size()==0){
@@ -62,8 +62,6 @@ public:
         int n=s.size(), i=0;
         int m = n;
         string ans ="";
-        // while(n-1>=0 && s[n-1]!=']'){n--;}
-        // if(n<0){return s;}
         while(i<n){
             if(s[i]>='0' && s[i]<='9'){
                 ans+=fun(s, i, n);
@@ -73,10 +71,6 @@ public:
             }
             i++;
         }
-        // while(n<m){
-        //     ans.push_back(s[n]);
-        //     n++;
-        // }
         return ans;
     }
 };
