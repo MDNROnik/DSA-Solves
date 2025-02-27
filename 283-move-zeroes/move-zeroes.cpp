aaff6ed566241f0af0846d59a4e21d6c6a35f939
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-02-27 14:26:27 +0600

    Time: 43 ms (8.71%) | Memory: 24 MB (17.95%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             if(nums[i]==0){
                 int j=i+1;
-                while(j<nums.size()){
+                while(j<n){
                     if(nums[j]!=0){
                         nums[i]=nums[j];nums[j]=0;break;
                     }
