7eefcc78a400801f50e9b0beb60f25092458a099
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-01 10:27:40 +0600

    Time: 0 ms (100.00%) | Memory: 30.8 MB (71.34%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.cpp
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool fun(vector<int>& nums, int i, int &n, int count){
-        // if(i==n){
-        //     return true;
-        // }
         if(i+1<n){
             if(nums[i]<=nums[i+1]){
                 return fun(nums, i+1, n, count);
@@ -13,13 +10,13 @@ public:
                     return false;
                 }
                 if( (i-1>=0 && nums[i-1]<=nums[i+1]) || i==0){
-                    int temp = nums[i];
-                    nums[i]=nums[i+1];
+                    // int temp = nums[i];
+                    // nums[i]=nums[i+1];
                     bool now = fun(nums, i+1, n, count+1);
                     if(now==true){
                         return true;
                     }
-                    nums[i]=temp;
+                    // nums[i]=temp;
                 }
                 nums[i+1]=nums[i];
                 return fun(nums, i+1, n, count+1);
