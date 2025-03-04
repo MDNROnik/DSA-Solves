9fb73ac437bc06650e39327b7a068fce65b82739
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-04 11:35:59 +0600

    Time: 1199 ms (25.42%) | Memory: 9.3 MB (40.88%) - LeetSync

---
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+bool checkPalindrome(string s)
+{
+    int i=0;int j=s.size()-1;
+    while(i<=j){
+        if(s[i]==s[j]){
+            i++;j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+    bool fun( string &s, string &a, string &b, int indexi, int indexj, int &n, int &ans, int index){
+        if(index==n){
+            bool x = checkPalindrome(a);
+            bool y = checkPalindrome(b);
+            if(x==true && y==true){
+                ans = max(ans, (  indexi*indexj  ));
+            }
+            return 1;
+        }
+        //not take
+        bool notTake = fun(s, a, b, indexi, indexj, n, ans, index+1);
+
+        //take by a
+        a.push_back(s[index]);
+        bool takeByA = fun(s, a, b, indexi+1, indexj, n, ans, index+1);
+        a.pop_back();
+
+        //take by 
+        b.push_back(s[index]);
+        bool takeByB = fun(s, a, b, indexi, indexj+1, n, ans, index+1);
+        b.pop_back();
+
+        return 1;
+
+    }
+    int maxProduct(string s) {
+        string a="", b="";
+        int indexi=0, indexj=0;
+        int n=s.size();
+        int ans=0;
+        fun(s, a, b, indexi, indexj, n, ans, 0);
+        return ans;
+    }
+};
