086a6f00cdc1d6656ca1304745c4fdcf075f44ab
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-08 06:05:18 +0600

    Time: 3 ms (85.67%) | Memory: 13.1 MB (78.01%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -4,11 +4,8 @@ public:
         vector<int>v(26, 0);
         int n = s.size(), ans = 0;
         for(int i=0;i<n;i++){
-            int a = s[i];
             int ascii = s[i]-97;
-            // cout<<i<<" "<<ascii<<" "<<v[ascii];
             if( v[ascii] == 1 ){
-                // cout<<" 1 IN"<<endl;
                 for(int j=0;j<26;j++){
                     v[j]=0;
                 }
@@ -16,7 +13,6 @@ public:
                 ans++;
             }
             else{
-                // cout<<" 2 IN "<<endl;
                 v[ascii] = 1;
             }
         }
