17242ca2ec659f98c316a96ae393bc1064b8f09f
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-10 12:25:36 +0600

    Time: 43 ms (13.84%) | Memory: 88.6 MB (21.81%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int ans = 0;
+        map<int, pair<int,int> >mp;  
+        int n = nums.size();
+        int now = 0, zero=0, one=0;
+        int first = nums[0];
+        for(int i=0;i<n;i++){
+
+            if(nums[i]==first){now++;zero++;}
+            else{now--;one++;}
+            // cout<<now<<endl;
+            if(now==0){
+                ans = max(ans, (i+1));
+            }
+            pair<int,int>p=mp[now];
+            if(p.first==0){
+                p.first = i+1;
+            }
+            else{
+                p.second = i+1;
+                ans = max(ans, (p.second-p.first));
+            }
+            mp[now]=p;
+        }
+        if(zero==one){
+            ans = max(ans, (zero+one));
+        }
+        return ans;
+    }
+};
