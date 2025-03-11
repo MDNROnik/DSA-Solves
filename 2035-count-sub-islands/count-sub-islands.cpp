9c5dfbc15a0b213b5774069c27271d775865b402
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-11 06:46:48 +0600

    Time: 146 ms (19.84%) | Memory: 234.7 MB (7.38%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -3,42 +3,41 @@ public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int m=grid1.size();
         int n=grid1[0].size();
-        vector<vector<int>>visited(m, vector<int>(n,0));
+        vector<vector<int>>visited(m, vector<int>(n,1));
         vector<vector<int>>visited2(m, vector<int>(n,0));
         int ans=0, first=0, second=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid1[i][j]==1 && visited[i][j]==0){
-                    queue<pair<int,int>>q;first++;
-                    q.push({i, j});
-                    visited[i][j]=1;
-                    while(!q.empty()){
-                        int row = q.front().first;
-                        int col = q.front().second;
-                        // cout<<row<<" "<<col<<endl;
-                        q.pop();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(grid1[i][j]==1 && visited[i][j]==0){
+        //             queue<pair<int,int>>q;first++;
+        //             q.push({i, j});
+        //             visited[i][j]=1;
+        //             while(!q.empty()){
+        //                 int row = q.front().first;
+        //                 int col = q.front().second;
+        //                 q.pop();
 
-                        if(row+1<m && visited[row+1][col]==0 && grid1[row+1][col]==1){
-                            q.push({row+1, col});
-                            visited[row+1][col]=1;
-                        }
-                        if(row-1>=0 && visited[row-1][col]==0 && grid1[row-1][col]==1){
-                            q.push({row-1, col});
-                            visited[row-1][col]=1;
-                        }
+        //                 if(row+1<m && visited[row+1][col]==0 && grid1[row+1][col]==1){
+        //                     q.push({row+1, col});
+        //                     visited[row+1][col]=1;
+        //                 }
+        //                 if(row-1>=0 && visited[row-1][col]==0 && grid1[row-1][col]==1){
+        //                     q.push({row-1, col});
+        //                     visited[row-1][col]=1;
+        //                 }
 
-                        if(col+1<n && visited[row][col+1]==0 && grid1[row][col+1]==1){
-                            q.push({row, col+1});
-                            visited[row][col+1]=1;
-                        }
-                        if(col-1>=0 && visited[row][col-1]==0 && grid1[row][col-1]==1){
-                            q.push({row, col-1});
-                            visited[row][col-1]=1;
-                        }
-                    }
-                }
-            }
-        }
+        //                 if(col+1<n && visited[row][col+1]==0 && grid1[row][col+1]==1){
+        //                     q.push({row, col+1});
+        //                     visited[row][col+1]=1;
+        //                 }
+        //                 if(col-1>=0 && visited[row][col-1]==0 && grid1[row][col-1]==1){
+        //                     q.push({row, col-1});
+        //                     visited[row][col-1]=1;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 bool IN=false, makeIsland=true;
@@ -51,7 +50,6 @@ public:
                     while(!q.empty()){
                         int row = q.front().first;
                         int col = q.front().second;
-                        // cout<<row<<" "<<col<<endl;
                         q.pop();
 
                         if(row+1<m && visited2[row+1][col]==0 && grid2[row+1][col]==1){
@@ -78,16 +76,12 @@ public:
                     }
                 }
                 if(IN==true && makeIsland==true){
-                    // cout<<i<<" "<<j<<endl;
-                    // cout<<"YES"<<endl;
+
                     ans++;
                 }
             }
         }
 
-        //second graph
-        
-        // cout<<first<<" "<<second<<endl;
         return ans;
     }
 };
