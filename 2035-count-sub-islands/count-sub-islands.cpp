89a8f7c5ed0271af0609ea43eb141014785df1c5
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-11 06:55:33 +0600

    Time: 130 ms (25.65%) | Memory: 221.7 MB (15.69%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int m=grid1.size(),n=grid1[0].size() , ans=0;
-
-        vector<vector<int>>visited2(m, vector<int>(n,0));
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 bool IN=false, makeIsland=true;
-                if(grid2[i][j]==1 && visited2[i][j]==0){
+                if(grid2[i][j]==1){
                     queue<pair<int,int>>q;
                     IN=true;
                     q.push({i, j});
