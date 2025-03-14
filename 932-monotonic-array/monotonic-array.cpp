b2e1126d5321015d849921a72c2013c2ca92becf
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-14 09:43:24 +0600

    Time: 0 ms (100.00%) | Memory: 100.3 MB (66.40%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -4,9 +4,7 @@ public:
         int n=nums.size();
         int i=0, pattern = 0;
         if(n==1){return true;}
-        else if(n==2){
-            return true;
-        }
+        else if(n==2){ return true; }
         else {
             for(i=0;i<n-1;i++){
                 if(nums[i]==nums[i+1]){continue;}
@@ -14,12 +12,9 @@ public:
             }
             if(i < n-1){
                 if(nums[i]<=nums[i+1]){pattern = 1;}
-                else{
-                    pattern = 2;
-                }
+                else{ pattern = 2; }
             }
         }
-        if(pattern == 0){return true;}
         while( i < n-1 ){
             if(pattern==1 && nums[i]<=nums[i+1]){
                 
