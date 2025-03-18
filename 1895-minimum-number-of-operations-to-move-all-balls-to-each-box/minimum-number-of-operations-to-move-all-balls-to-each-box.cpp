0780ce91db643dcf24fad500a79b203537c1ea1c
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-19 01:09:21 +0600

    Time: 1 ms (66.53%) | Memory: 12.7 MB (24.56%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -12,18 +12,19 @@ public:
             }
             pre[i] = (steps);
         }
+        vector<int>ans(n,0);
         count=0, steps=0;
         for(int i=n-1;i>=0;i--){
             steps+=count;
             if(boxes[i]=='1'){
                 count++;
-            }
+            }ans[i] = (pre[i]+steps);
             suf[i] = (steps);
         }
-        int j=0;vector<int>ans;
-        for(int i=0;i<n;i++){
-            ans.push_back(pre[i]+suf[i]);
-        }
+        // int j=0;vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     ans.push_back(pre[i]+suf[i]);
+        // }
 
         
         return ans;
