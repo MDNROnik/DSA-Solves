5373a2f4d484b3492dca06e3359ebbbaa71ce303
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-19 22:43:42 +0600

    Time: 6 ms (64.84%) | Memory: 140 MB (74.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -2,16 +2,7 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n=nums.size(),zero=0;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==0){
-        //         zero++;
-        //     }
-        // }
-        // cout<<zero<<endl;
-        // if(zero%3==1 ){
-        //     return -1;
-        // }
-        int ans =INT_MAX, now=0;
+        int  now=0;
         for(int i=0;i<n-2;i++){
             if(nums[i]==0){
                 if(nums[i]==0){nums[i]=1;}
@@ -20,14 +11,7 @@ public:
                 now++;
             }
         }
-
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zero++;
-            }
-        }
-        if(zero>0){return -1;}
-        ans = min(ans, now);
-        return ans;
+        if(nums[n-1]==0 || nums[n-2]==0){return -1;}
+        return now;
     }
 };
