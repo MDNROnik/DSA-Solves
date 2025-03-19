7371ce506879b341bac6158d8515d73e68532b61
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-03-19 22:55:32 +0600

    Time: 8 ms (46.82%) | Memory: 139.4 MB (15.41%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>less, great;
+        int n=nums.size();
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                less.push_back(nums[i]);
+            }
+            else if(nums[i]==pivot){
+                c++;
+            }
+            else{
+                great.push_back(nums[i]);
+            }
+        }
+        vector<int>ans;
+        ans=less;
+        while(c--){
+            ans.push_back(pivot);
+        }
+        n=great.size();
+        for(int i=0;i<n;i++){
+            ans.push_back(great[i]);
+        }
+        return ans;
+    }
+};
