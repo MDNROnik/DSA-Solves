bfe84fecebde119db18b0ec1036233f148cc4425
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2025-04-13 10:39:08 +0600

    Time: 17 ms (69.65%) | Memory: 107.2 MB (61.87%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int n=garbage.size();
-        int ansp = 0, ansg=0, ansm=0;
+        int ans=0;
         int travelp=0, travelg=0, travelm=0;
         for(int i=0;i<n;i++){
             int g=0, p=0, m=0;
@@ -12,31 +12,29 @@ public:
                 if(garbage[i][j]=='P'){p++;}
                 if(garbage[i][j]=='M'){m++;}
             }
+            ans+=(g+p+m);
             if(i==0){
-                ansp+=p;
-                ansg+=g;
-                ansm+=m;
+                
             }
             else{
                 travelp+=travel[i-1];
                 travelg+=travel[i-1];
                 travelm+=travel[i-1];
                 if(p>0){
-                    ansp+=(p+travelp);
+                    ans+=(travelp);
                     travelp=0;
                 }
                 if(g>0){
-                    ansg+=(g+travelg);
+                    ans+=(travelg);
                     travelg=0;
                 }
                 if(m>0){
-                    ansm+=(m+travelm);
+                    ans+=(travelm);
                     travelm=0;
                 }
             }
         }
-        // cout<<ansp<<" "<<ansg<<" "<<ansm<<endl;
-        int ans=ansp+ansg+ansm;
+
         return ans;
     }
 };
