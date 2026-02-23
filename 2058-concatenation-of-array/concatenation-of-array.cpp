975235413e7c512215653a05e9dbb37f035f975f
Md. Nimur Rahman Onik <136170992+MDNROnik@users.noreply.github.com>
2026-02-23 09:42:46 +0100

    Time: 3 ms (10.25%) | Memory: 17 MB (28.47%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& v) {
-        v.insert(v.end(), v.begin(), v.end());
-        return v;
+    vector<int> getConcatenation(vector<int>& nums) {
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        return nums;
     }
 };
